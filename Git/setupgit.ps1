85bcea1cbc2fd1df15d343f8d9be0ba5e6dc6587
brian.smith <brian.smith@asolutions.com>
2014-08-25 22:16:25 -0500

    added git stuffs - BS

---
+++ Git/setupgit.ps1
@@ -0,0 +1,6 @@
+git config --replace-all alias.ufetch "!sh ./Resources/Git/setusername && sh ./Resources/Git/change_origin && git fetch"
+git config alias.st status
+git config alias.br branch
+git config alias.co checkout
+git config alias.ci commit
+
